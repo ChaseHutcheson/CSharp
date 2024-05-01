1c4c6f1b38a0c0fb038bcb641e7d0272a398e931
Chase Hutcheson <111781666+ChaseHutcheson@users.noreply.github.com>
2024-05-01 01:43:31 -0400

    Starting First Notes

---
+++ C# Notes/1.) Variables/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp_Notes.C__Notes._1.__Variables
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Write line writes the text with a newline.
+            Console.WriteLine("Hello, World");
+
+            // Readline prevents the window from closing so
+            // the message can be displayed
+            Console.ReadLine();
+        }
+    }
+}
---
+++ C# Projects/Drawing a Shape/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp_Notes.C__Projects.Drawing_a_Shape
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Write line writes the text with a newline.
+            Console.WriteLine("Hello, World");
+
+            // Readline prevents the window from closing so
+            // the message can be displayed
+            Console.ReadLine();
+        }
+    }
+}
